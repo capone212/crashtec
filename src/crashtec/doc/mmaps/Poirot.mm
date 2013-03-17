42f8fadc7706e7714500f3f66759bd2b3e17e88d
anzor.apshev <anzor.apshev@gmail.com>
2013-03-17 23:34:56 +0400

    mmind maps

---
+++ src/crashtec/doc/mmaps/Poirot.mm
@@ -0,0 +1,20 @@
+<map version="0.9.0">
+<!-- To view this file, download free mind mapping software FreeMind from http://freemind.sourceforge.net -->
+<node CREATED="1336066519791" ID="ID_476892604" MODIFIED="1336067888277" TEXT="Poirot">
+<node CREATED="1336067825609" ID="ID_1565933669" MODIFIED="1336067885681" POSITION="right" TEXT="dump reporter">
+<node CREATED="1336281817445" ID="ID_1619409184" MODIFIED="1336281948653" TEXT="should not be implemented, or implemented with breakpad"/>
+</node>
+<node CREATED="1336067896686" ID="ID_84569780" MODIFIED="1336067934830" POSITION="left" TEXT="dump dispatcher">
+<node CREATED="1336282040711" ID="ID_403211679" MODIFIED="1336282046730" TEXT="Set API"/>
+</node>
+<node CREATED="1336067974321" ID="ID_376104964" MODIFIED="1357148012633" POSITION="right" TEXT="dump processer">
+<node CREATED="1336067999798" ID="ID_732103317" MODIFIED="1336068007397" TEXT="plugins"/>
+</node>
+<node CREATED="1336068015457" ID="ID_1628950696" MODIFIED="1357148021025" POSITION="left" TEXT="Result analyzer IA">
+<node CREATED="1336068030121" ID="ID_62026812" MODIFIED="1336068035421" TEXT="analyzer plugins"/>
+</node>
+<node CREATED="1336068046560" ID="ID_930593716" MODIFIED="1357148015264" POSITION="right" TEXT="action handler">
+<node CREATED="1336068078514" ID="ID_222899690" MODIFIED="1336068085114" TEXT="assigning to user"/>
+</node>
+</node>
+</map>
