2d9fce5d923fafcb68231a450bf4177220bb2cbd
anzor.apshev <anzor.apshev@gmail.com>
2013-05-12 17:42:20 +0400

    db routines refactored
    - refactored basic algorithms for provider
    - added smart Record type to obtain diff's

--- src/crashtec/db/provider/routines.py
+++ src/crashtec/db/provider/routines.py
@@ -7,30 +7,78 @@ Provides acces to db.
 '''
 
 import psycopg2
+import itertools
 from psycopg2.extras import RealDictCursor
 from crashtec.config import dbconfig
 from crashtec.db.schema.fields import PRIMARY_KEY_FIELD
+
+from crashtec.utils.exceptions import CtCriticalError
 
 #TODO: think about error handling for these queries 
 
 # FIXME: replace simple dict with smart one, 
 # which should remember old initial values. 
-# Update routine must use this to filter unchanged values, this should 
-# reduce amount of data collisions.
-Record = dict
+
+class Record(object):
+    '''
+        Represents database table record (single row)
+        
+    '''
     
+    def __init__(self, value = dict()):
+        '''
+        Constructor. 
+            
+        Takes dictionary of initial values, where keys are 
+        table field names and mapped values are db field values of current row. 
+        '''
+        # Holds two copies of initial map, they used to track changed values
+        self.initial_values = value.copy()
+        self.current_values = value.copy()
+    
+    def __eq__(self, other):
+        return  self.current_values == other.current_values
+    
+    def __setitem__(self, key, value):
+        self.current_values[key] = value
+    
+    def __getitem__(self, key):
+        return self.current_values[key]
+    
+    def iteritems(self):
+        return self.current_values.iteritems()
+    
+    def keys(self):
+        return self.current_values.keys()
+    
+    def values(self):
+        return self.current_values.values()
+    
+    def updated_values(self):
+        ''' 
+            Returns a dictionary of updated items.
+            
+            'Updated items' are items that does not exists or changed 
+            in initial map (that was passed in constructor)
+        '''
+        curents = self.current_values
+        olds = self.initial_values
+        return {k : v for k, v in curents.iteritems() 
+                if (k not in olds) or (v != olds[k])}
+        
+         
 class Cursor(object):
     def __init__(self, cursor_impl):
         self._cursor_impl = cursor_impl
     
     def fetch_one(self):
-        return self._cursor_impl.fetchone()
+        return Record(self._cursor_impl.fetchone())
     
     def fetch_many(self, count):
-        return self._cursor_impl.fetchmany(count)
+        return (Record(x) for x in self._cursor_impl.fetchmany(count))
     
     def fetch_all(self):
-        return self._cursor_impl.fetchall()
+        return (Record(x) for x in self._cursor_impl.fetchall())
 
 def exec_sql(sql, params):
 #    with psycopg2.connect(DSN) as connection:
@@ -42,19 +90,14 @@ def exec_sql(sql, params):
     cursor = connection.cursor(cursor_factory=RealDictCursor);
     cursor.execute(sql, params)
     return Cursor(cursor)
+ 
     
 # param record is instance of class Record
 def create_new_record(table, record):
-    fields = str()
-    placeholders = str()
-    arguments = ()
-    for property_id, value in record.iteritems():
-        if (fields) :
-            fields += ', '
-            placeholders += ', '
-        fields += property_id
-        placeholders += "%s"
-        arguments += (value,)
+    # TODO: replace tuples with list!
+    fields = ', '.join(record.keys())
+    placeholders = ', '.join(["%s" for x in range(len(record.keys()))])
+    arguments = record.values()
     sql = "INSERT INTO %s (%s) VALUES (%s);" % (table, fields, placeholders)
     #TODO: log and adapt exception
     exec_sql(sql, arguments)
@@ -70,21 +113,19 @@ def select_from(table_name, field_list=[],  db_filter=None, order=None):
     
     if (order):
         sql = "%s order by %s" % (sql, order.to_sql())
-    #print sql , ' | ' , params
     return exec_sql(sql, params)
 
-# TODO: add optional id parameter
 def update_record(table, record, key_field = PRIMARY_KEY_FIELD):
-    arguments = ()
-    fields = str()
-    for property_id, value in record.iteritems():
-        if property_id == key_field:
-            continue
-        if (fields) :
-            fields += ', '
-        fields += '%s=%%s' % property_id
-        arguments += (value,)
+    updated_values = record.updated_values()
+    if not updated_values:
+        return
+    if key_field in updated_values.keys():
+        raise CtCriticalError('Key field value is updated.' 
+        'This should be a bug in called module')
+    
+    fields = ', '.join('%s=%%s' %
+                       property_id for property_id in updated_values.keys())
+    arguments = updated_values.values()
     sql = 'update %s SET %s WHERE %s = %%s' % (table, fields, key_field)
-    arguments += (record[key_field],)
-    #print sql, ' | ' , arguments
+    arguments.append(record[key_field])
     return exec_sql(sql, arguments)
---
+++ src/crashtec/db/provider/test/testroutines.py
@@ -0,0 +1,121 @@
+'''
+Created on 12.05.2013
+
+@author: capone
+'''
+import unittest
+from mock import patch
+
+from crashtec.db.provider.routines import Record
+from crashtec.db.provider import routines
+from crashtec.utils.exceptions import CtCriticalError
+
+def _get_sample_record():
+    return {'key1' : 'value2', 'key2' : 'value2' }
+
+class TestRecord(unittest.TestCase):
+    def test01_get_value(self):
+        record = Record(_get_sample_record())
+        for key, value in _get_sample_record().iteritems():
+            self.assertEqual(value, record[key], 'Getter does not work')
+    
+    def test02_set_values(self):
+        record = Record()
+        for key, value in _get_sample_record().iteritems():
+            record[key] = value
+        
+        for key, value in _get_sample_record().iteritems():
+            self.assertEqual(value, record[key], 'Setter does not work')
+    
+    def test03_update(self):
+        record = Record(_get_sample_record())
+        record['mock_key'] = 'mock_value' 
+        
+        for key, value in _get_sample_record().iteritems():
+            self.assertEqual(value, record[key], 'Setter does not work')
+        self.assertEqual('mock_value', record['mock_key'],
+                          'Setter does not work')
+    
+    def test04_updated_values(self):
+        record = Record(_get_sample_record())
+        
+        initial = _get_sample_record()
+        modifier = {initial.keys()[1] : 'garbage', 'mock_key' : 'mock_value'}
+        for key, value in modifier.iteritems():
+            record[key] = value
+        updated_values = record.updated_values()
+        self.assertEqual(updated_values, modifier)
+        
+        # Modify second time
+        modifier2 = {initial.keys()[0] : 'garbage2: reload', 
+                     'mock_key2' : 'mock_value2'}
+        for key, value in modifier2.iteritems():
+            record[key] = value
+            
+        # Validate
+        modifier2.update(modifier)
+        updated_values = record.updated_values()
+        self.assertEqual(updated_values, modifier2)
+
+
+@patch('crashtec.db.provider.routines.exec_sql')
+class Test_create_new_record(unittest.TestCase):
+    def test_with_dictionary(self, pached_exec_sql):
+        TABLE_NAME = 'mock_table'
+        mock_record = {'field1' : 'value1', 'field2' : 'value2'}
+        routines.create_new_record(TABLE_NAME, mock_record)
+        EXPECTED_SQL = 'INSERT INTO mock_table (field2, field1) VALUES (%s, %s);'
+        
+        # Check results
+        (sql_string, values), keywords = pached_exec_sql.call_args
+        self.assertEqual(EXPECTED_SQL, sql_string,'sql strings does not match')
+        self.assertEqual(list(mock_record.values()),
+                        list(values))
+    def test_with_Record(self, pached_exec_sql):
+        TABLE_NAME = 'mock_table'
+        mock_record = {'field1' : 'value1', 'field2' : 'value2'}
+        routines.create_new_record(TABLE_NAME, Record(mock_record))
+        EXPECTED_SQL = 'INSERT INTO mock_table (field2, field1) VALUES (%s, %s);'
+        
+        # Check results
+        (sql_string, values), keywords = pached_exec_sql.call_args
+        self.assertEqual(EXPECTED_SQL, sql_string,'sql strings does not match')
+        self.assertEqual(list(mock_record.values()),
+                        list(values))
+
+
+@patch('crashtec.db.provider.routines.exec_sql')
+class Test_update_record(unittest.TestCase):
+
+    def test_key_field_updated(self, pached_exec_sql):
+        record = Record()
+        for key, value in self.get_mock_record().iteritems():
+            record[key] = value 
+        with self.assertRaises(CtCriticalError):
+            self._do_test(record, pached_exec_sql)
+        
+    
+    def test_no_updated_values(self, pached_exec_sql):
+        self._do_test(Record(self.get_mock_record()), pached_exec_sql)
+        self.assertFalse(pached_exec_sql.called, 'Should not be called')
+    
+    def test_partial_updated(self, pached_exec_sql):
+        record = Record(self.get_mock_record())
+        MOCK_VALUE = 'mock_value'
+        record['field2'] = MOCK_VALUE
+        (sql_string, values), keywords = self._do_test(record, pached_exec_sql)
+        # Check results
+        EXPECTED_SQL = 'update mock_table SET field2=%s WHERE id = %s'
+        self.assertEqual(EXPECTED_SQL, sql_string)
+        self.assertEqual([MOCK_VALUE, record['id']], list(values))
+    
+    def _do_test(self, mock_record, pached_exec_sql):
+        MOCK_TABLE_NAME = 'mock_table'
+        routines.update_record(MOCK_TABLE_NAME, mock_record)
+        return pached_exec_sql.call_args
+
+    def get_mock_record(self):
+        return {'id' : 10, 'field1' : 'value1', 'field2' : 'value2'}
+    
+if __name__ == '__main__':
+    unittest.main()
